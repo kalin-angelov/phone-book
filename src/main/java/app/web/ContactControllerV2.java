e9610b3a80d39137c7f36fb975770b8b23cd19ba
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-07-21 17:22:36 +0300

    edit: add more request in the contact controller

--- src/main/java/app/web/ContactControllerV2.java
+++ src/main/java/app/web/ContactControllerV2.java
@@ -1,16 +1,21 @@
 package app.web;
 
 import app.model.Contact;
+import app.model.ContactActivity;
+import app.model.ContactType;
+import app.model.SearchCriteria;
 import app.serivce.ContactService;
 import app.web.dto.ContactEditRequest;
 import app.web.dto.ContactRequest;
 import app.web.dto.ContactResponse;
+import app.web.dto.SearchResponse;
 import app.web.mapper.DtoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,6 +24,56 @@ public class ContactControllerV2 {
 
     @Autowired
     private ContactService contactService;
+
+    @GetMapping
+    public ResponseEntity<List<Contact>> getAllContacts() {
+
+        List<Contact> contactList = contactService.getAllContacts();
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(contactList);
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<SearchResponse> getContactByContactType(@PathVariable ContactType type) {
+
+        List<Contact> result = contactService.getAllContactsByType(type);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SearchResponse.builder()
+                        .status(HttpStatus.OK.value())
+                        .result(result)
+                        .build());
+    }
+
+    @GetMapping("/{activity}")
+    public ResponseEntity<SearchResponse> getContactByContactActivity(@PathVariable ContactActivity activity) {
+
+        List<Contact> result = contactService.getAllContactsByActivity(activity);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SearchResponse.builder()
+                        .status(HttpStatus.OK.value())
+                        .result(result)
+                        .build());
+    }
+
+
+    @GetMapping("/{criteria}/{search}")
+    public ResponseEntity<SearchResponse> getContactByCriteriaAndSearch(@PathVariable SearchCriteria criteria, @PathVariable String search) {
+
+        List<Contact> result = contactService.getAllContactsBy(criteria, search);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(SearchResponse.builder()
+                        .status(HttpStatus.OK.value())
+                        .result(result)
+                        .build());
+    }
 
     @PostMapping("/new")
     public ResponseEntity<ContactResponse> createNewContact(@RequestBody ContactRequest contactRequest) {
