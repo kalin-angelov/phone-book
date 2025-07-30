c66efb3022c9afa774e71ee1a519c3413ad6da33
Kalin Angelov <kalin.st.angelov@gmail.com>
2025-07-30 12:47:57 +0300

    edit: add type field and NotBlank annotation for the firstName and phoneNumber

--- src/main/java/app/web/dto/ContactEditRequest.java
+++ src/main/java/app/web/dto/ContactEditRequest.java
@@ -1,5 +1,8 @@
 package app.web.dto;
 
+import app.model.ContactType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,9 +10,17 @@ import lombok.Data;
 @Builder
 public class ContactEditRequest {
 
+    @NotBlank
     private String firstName;
+
     private String lastName;
+
     private String region;
+
     private String avatar;
+
+    @NotBlank
     private String phoneNumber;
+
+    private ContactType type;
 }
